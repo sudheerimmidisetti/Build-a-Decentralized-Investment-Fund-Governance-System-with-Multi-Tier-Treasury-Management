df0d926773208ffc4124fbf999e3990c5bb48201
sudheerimmidisetti <sudheerimmidisetti@gmail.com>
2026-01-16 11:56:00 +0530

    Configure Hardhat for smart contract development

---
+++ hardhat.config.ts
@@ -0,0 +1,22 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+};
+
+export default config;
